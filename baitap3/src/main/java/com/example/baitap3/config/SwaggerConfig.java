d4062771f75b8b3d09183fb7eb0011ad7c739fd3
Nhuhuynh191004 <HuynhNTNCE181566@fpt.edu.vn>
2025-09-26 11:31:19 +0700

    Swagger

---
+++ baitap3/src/main/java/com/example/baitap3/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.baitap3.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Baitap3 API")
+                        .version("1.0")
+                        .description("API documentation for Baitap3 project"));
+    }
+}
